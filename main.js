6c1a9e439592f7351c1d28e49b485a0dba4f912b
Pillroy <walkertom1996@gmail.com>
2021-04-23 17:47:54 +0100

    TASKS 1-5 COMPLETE

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Task 1
+
 // change these variables to test your code
-let playerMove = "rock";
+let playerMove = "paper";
 let computerMove = "scissors";
 
 /*POSSIBLE COMBINATIONS:
@@ -25,3 +27,66 @@ if ((playerMove === "rock" && computerMove === "rock") || (playerMove === "sciss
     console.log("COMPUTER WINS");
 }
 
+
+// Task 2
+
+function getWinner(playerMove, computerMove) {
+    if ((playerMove === "rock" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "paper")) {
+        return 0;
+    } else if ((playerMove === "rock" && computerMove === "scissors") || (playerMove === "scissors" && computerMove === "paper") || (playerMove === "paper" && computerMove === "rock")) {
+        return 1;
+    } else if ((playerMove === "rock" && computerMove === "paper") || (playerMove === "scissors" && computerMove === "rock") || (playerMove === "paper" && computerMove === "scissors")) {
+        return -1;
+    }
+}
+
+let result = getWinner("paper", "scissors");
+
+console.log(result);
+
+
+// Task 3
+
+// playerMove = prompt("Name your move: rock, paper or scissors");
+
+// result = getWinner(playerMove, "paper");
+
+// alert(result);
+
+
+// Task 4
+
+function generateMove() {
+    let intChoice = Math.floor(Math.random() * 3);
+    if (intChoice === 0) {
+      return computerMove = "rock";
+    } else if (intChoice === 1) {
+        return computerMove = "paper";
+    } else if (intChoice === 2) {
+        return computerMove = "scissors";
+    }
+}
+
+playerMove = prompt("Name your move: rock, paper or scissors");
+
+generateMove();
+
+result = getWinner(playerMove, computerMove);
+
+alert(result);
+
+
+// Task 5
+
+while(true) {
+    playerMove = prompt("Name your move: rock, paper or scissors");
+    generateMove();
+    result = getWinner(playerMove, computerMove);
+    alert(result);
+    let playAgain = window.confirm("Play Again?");
+    if (playAgain !== true) {
+        break;
+    }
+}
+
+// Task 6
